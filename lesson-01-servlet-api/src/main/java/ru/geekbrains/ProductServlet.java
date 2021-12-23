4a8910351030addc740241aefee3c4abdad16f5f
vtb50636271 <aepetrov@vtb.ru>
2021-12-23 18:06:19 +0300

    - Урок 1. Основы Java EE

--- lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
+++ lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
@@ -25,7 +25,19 @@ public class ProductServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter wr = resp.getWriter();
         if (req.getParameter("id") != null) {
-            // TODO
+            String id = req.getParameter("id");
+            Product product = null;
+            try {
+                product = productRepository.findById(Long.parseLong(id));
+            } catch (NumberFormatException ignored) {
+            }
+            if(product != null) {
+                wr.println(String.format("<h1>ID = %d</h1>", product.getId()));
+                wr.println(String.format("<h3>%s</h3>", product.getName()));
+            } else {
+                wr.println(String.format("<h1>Error. Not found the product with id =  %s</h1>", id));
+            }
+
         } else if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
             wr.println("<table>");
 
@@ -37,8 +49,7 @@ public class ProductServlet extends HttpServlet {
             for (Product product : productRepository.findAll()) {
                 wr.println("<tr>");
                 wr.println("<td>" + product.getId() + "</td>");
-                wr.println("<td>" + product.getName() + "</td>");
-                // TODO <a href='product?id=12'></a>
+                wr.println(String.format("<td><a href='product?id=%d'>%s</a></td>", product.getId(), product.getName()));
                 wr.println("</tr>");
             }
 
